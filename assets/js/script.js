28401870d0d8c01f332a90007b193528d3dc30d3
Ross-Fraser <144271273+Ross-Fraser@users.noreply.github.com>
2023-12-13 18:08:29 +0000

    create: add DOM content loaded event listener

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+// waits for the DOM to load before running the game
+document.addEventListener("DOMContentLoaded", function () {
+  const choices = ["Rock", "Paper", "Scissors", "Lizard", "Spock"]; // Possible moves for the game
+  let playerScore = 0; // Keeps track of the players score
+  let computerScore = 0; // Keeps track of the computers score
+
