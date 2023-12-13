5e7e3617f68bf201b9d567301c7aad62ca01d08b
Ross-Fraser <144271273+Ross-Fraser@users.noreply.github.com>
2023-12-13 18:12:32 +0000

    create: hide start buttons and display game content

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,35 @@ document.addEventListener("DOMContentLoaded", function () {
   let playerScore = 0; // Keeps track of the players score
   let computerScore = 0; // Keeps track of the computers score
 
+  // Hides the start buttons
+  function hideStartButtons() {
+    const btnStartGame = document.getElementById("btnStartGame");
+    const btnInstructions = document.getElementById("btnInstructions");
+    btnStartGame.style.display = "none";
+    btnInstructions.style.display = "none";
+  }
+
+  // Shows the game content
+  function showGameContent() {
+    const btnsPlayerChoice = document.getElementById("btnsPlayerChoice");
+    const scoreboard = document.getElementById("scoreboard");
+    const results = document.getElementById("results");
+    const btnReturnGame = document.getElementById("btnReturnGame");
+    btnsPlayerChoice.style.display = "block";
+    scoreboard.style.display = "block";
+    results.style.display = "block";
+    btnReturnGame.style.display = "block";
+  }
+
+  // Computers choice is randomly selected from the choices
+  function computerPlay() {
+    return choices[Math.floor(Math.random() * choices.length)];
+  }
+
+  // Event listeners
+  document.getElementById("btnStartGame").addEventListener("click", () => {
+    hideStartButtons();
+    showGameContent();
+  });
+
+});
